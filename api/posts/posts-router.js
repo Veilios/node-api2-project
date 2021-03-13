9a968597af4bfe4c3f3f4a057b9033d54c342306
Alan Mir <alanpaulphotos@gmail.com>
2021-03-13 16:02:45 -0500

    Finished testing and revising code, Met MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -68,10 +68,10 @@ router.put('/:id', (req, res) => {
         .then(p => {
             if(!p) {
                 res.status(404).json({ message: "The post with the specified ID does not exist" });
-            } else if ( !p.title || !p.contents ) {
+            } else if ( !post.title || !post.contents ) {
                 res.status(400).json({ message: "Please provide title and contents for the post" });
             } else {
-                res.status(200).json(p);
+                res.status(200).json(post);
             };
         })
         .catch(err => {
@@ -117,3 +117,6 @@ router.get('/:id/comments', (req, res) => {
             res.status(500).json({ message: "The comments information could not be retrieved" });
         });
 });
+
+
+module.exports = router;
